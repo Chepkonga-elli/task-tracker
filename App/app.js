fb545511637c513ad4dab76976d9f1138595ef0b
Chepkonga-elli <kiptoochelimo219@gmail.com>
2023-01-15 14:16:48 +0300

    Added two more files

--- App/app.js
+++ App/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function index() {
+function aapp() {
   return (
     <div>
       
@@ -8,4 +8,4 @@ function index() {
   )
 }
 
-export default index
+export default aapp
